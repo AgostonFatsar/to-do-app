a13a76d656b10c06e28c0559cf4453c5f8f4fff4
agoston.fatsar <fatsar.agoston@gmail.com>
2023-05-22 15:33:25 +0200

    Add: getTasks and addTask endpoints

--- todo/src/main/java/com/project/todo/controller/TaskController.java
+++ todo/src/main/java/com/project/todo/controller/TaskController.java
@@ -1,11 +1,30 @@
 package com.project.todo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.todo.model.Task;
+import com.project.todo.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/task")
 public class TaskController {
 
-    public TaskController() {}
+    private final TaskService service;
+
+    @Autowired
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return service.getTasks();
+    }
+
+    @PostMapping
+    public void addTask(@RequestBody Task task) {
+        service.addTask(task);
+    }
 }
