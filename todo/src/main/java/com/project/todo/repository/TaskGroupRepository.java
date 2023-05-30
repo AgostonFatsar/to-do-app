0c719b2ebbc0539627b32f0ab545416f4e3281a2
agoston.fatsar <fatsar.agoston@gmail.com>
2023-05-30 14:25:12 +0200

    Add: TaskGroup repository

--- todo/src/main/java/com/project/todo/repository/TaskGroupRepository.java
+++ todo/src/main/java/com/project/todo/repository/TaskGroupRepository.java
@@ -1,12 +1,9 @@
 package com.project.todo.repository;
 
-import com.project.todo.model.Task;
+import com.project.todo.model.TaskGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
+public interface TaskGroupRepository extends JpaRepository<TaskGroup, Long> {
 }
