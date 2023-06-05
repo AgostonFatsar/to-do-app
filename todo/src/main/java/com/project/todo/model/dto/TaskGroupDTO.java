c7f4391dc8abdd63676188162e26b01bf438bba0
agoston.fatsar <fatsar.agoston@gmail.com>
2023-06-05 10:47:48 +0200

    Add: TaskGroupDTO

---
+++ todo/src/main/java/com/project/todo/model/dto/TaskGroupDTO.java
@@ -0,0 +1,18 @@
+package com.project.todo.model.dto;
+
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class TaskGroupDTO {
+
+    private Long id;
+    private String name;
+    private Set<Long> taskIds;
+}
