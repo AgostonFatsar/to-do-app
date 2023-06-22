8c5ca2c0c53d6a4b8597a13bcf762181dc2dded2
agoston.fatsar <fatsar.agoston@gmail.com>
2023-06-22 12:01:14 +0200

    Implement: UserDetailsService in UserService

--- todo/src/main/java/com/project/todo/data/UserRepository.java
+++ todo/src/main/java/com/project/todo/data/UserRepository.java
@@ -4,6 +4,11 @@ import com.project.todo.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
+
+
+    Optional<AppUser> findAppUserByUserName(String username);
 }
--- todo/src/main/java/com/project/todo/service/UserService.java
+++ todo/src/main/java/com/project/todo/service/UserService.java
@@ -2,16 +2,26 @@ package com.project.todo.service;
 
 import com.project.todo.data.UserRepository;
 import com.project.todo.model.AppUser;
+import com.project.todo.model.Role;
 import com.project.todo.model.dto.DtoFactory;
 import com.project.todo.model.dto.UserDTO;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class UserService {
+@NoArgsConstructor
+public class UserService implements UserDetailsService {
+
 
     UserRepository repository;
 
@@ -19,6 +29,7 @@ public class UserService {
     public UserService(UserRepository repository) {
         this.repository = repository;
     }
+
 
     public List<UserDTO> getUsers() {
         return repository.findAll().stream()
@@ -29,6 +40,7 @@ public class UserService {
         AppUser user = AppUser.builder()
                 .userName(userDto.getUserName())
                 .password(userDto.getPassword())
+                .role(Role.USER)
                 .build();
         repository.save(user);
     }
@@ -42,5 +54,13 @@ public class UserService {
 
     public void deleteUser(Long id) {
         repository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = repository.findAppUserByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        return new User(user.getUserName(), user.getPassword(), authorities);
     }
 }
