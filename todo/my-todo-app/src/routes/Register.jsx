2b3a3284ae8ebf58913386440dc33686d090a5b6
agoston.fatsar <fatsar.agoston@gmail.com>
2023-11-01 21:27:46 +0100

    Add: Register.jsx

---
+++ todo/my-todo-app/src/routes/Register.jsx
@@ -0,0 +1,48 @@
+import {useState} from "react";
+import {useNavigate} from 'react-router-dom';
+
+const Register = () => {
+
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('/api/auth/register', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({username, password})
+            });
+            console.log(response);
+            navigate('/dashboard');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input type="text" value={username} onChange={handleUsernameChange} />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={handlePasswordChange} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default Register;
